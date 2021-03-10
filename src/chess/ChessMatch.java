c442e308a0b90e35f62174d94340cf9f07cde958
JoaoPaulo-Costa <costa.joao.paulo93@gmail.com>
2021-03-10 14:28:41 -0300

    BoardException e programacao defensinva

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,9 +25,9 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2,7));
         board.placePiece(new King(board, Color.BLACK), new Position(0,4));
-        board.placePiece(new Rook(board, Color.WHITE), new Position(1,2));
+        board.placePiece(new Rook(board, Color.WHITE), new Position(1,5));
         board.placePiece(new King(board, Color.BLACK), new Position(7,5));
 
 
