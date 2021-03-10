a088f28658379610be2d6d5ad026ce650f3d3e99
JoaoPaulo-Costa <costa.joao.paulo93@gmail.com>
2021-03-10 13:38:51 -0300

    Implementando Peca e Tabuleiro

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,20 @@
+package boardgame;
+
+public class Piece {
+
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null; //java por padrão já faz isso
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+
+
+
+}
