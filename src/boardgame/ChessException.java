cbf1cfbd828b8574ae855af3e80d061c51b04b60
JoaoPaulo-Costa <costa.joao.paulo93@gmail.com>
2021-03-10 14:46:50 -0300

    ChessException e ChessPosition

---
+++ src/boardgame/ChessException.java
@@ -0,0 +1,9 @@
+package boardgame;
+
+public class ChessException extends RuntimeException{
+
+    public ChessException(String msg) {
+        super(msg);
+    }
+
+}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,13 +24,15 @@ public class ChessMatch {
         return mat;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2,7));
-        board.placePiece(new King(board, Color.BLACK), new Position(0,4));
-        board.placePiece(new Rook(board, Color.WHITE), new Position(1,5));
-        board.placePiece(new King(board, Color.BLACK), new Position(7,5));
-
-
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.BLACK));
+        placeNewPiece('h', 5, new Rook(board, Color.WHITE));
+        placeNewPiece('f', 1, new King(board, Color.WHITE));
     }
 
 }
